a1b29ed6f345089949bd94f1996d5776bb7d9cbd
Fürst <furstblumier@users.noreply.github.com>
2024-02-25 17:57:39 +0100

    sitemap stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
 import icon from "astro-icon";
 
@@ -15,5 +16,5 @@ export default defineConfig({
       protocol: "https"
     }]
   },
-  integrations: [tailwind(), react(), icon()]
+  integrations: [tailwind(), react(), icon(), sitemap()]
 });
